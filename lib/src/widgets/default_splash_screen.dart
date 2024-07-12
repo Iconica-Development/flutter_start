c558c8dbc7f719c264af302b52b1c8721afdab0f
mike doornenbal <mikedoornenbal9@icloud.com>
2024-07-12 16:06:01 +0200

    fix: default style and flutter_introduction to new version

--- lib/src/widgets/default_splash_screen.dart
+++ lib/src/widgets/default_splash_screen.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 Text defaultSplashScreen(BuildContext context) => Text(
       'iconinstagram',
-      style: Theme.of(context).textTheme.titleLarge,
+      style: Theme.of(context).textTheme.headlineLarge,
     );
